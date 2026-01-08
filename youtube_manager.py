2ae64c604ca721ef5d05e82a101610a766cec3f4
Pawan-Chahar <pavankuchahar@gmail.com>
2026-01-09 00:49:33 +0530

    skelton added

---
+++ youtube_manager.py
@@ -0,0 +1,75 @@
+import json 
+
+
+
+
+def load_data():
+    try:
+        with open('youtube.txt' , 'r') as file:
+            test = json.load(file)
+            print(type(test))
+            return test
+
+    except FileNotFoundError:
+        return []
+        
+
+def save_data_helper(videos):
+    with open('youtube.txt' , 'w') as file:
+        json.dump(videos, file)
+
+    
+
+def list_all_videos(videos):
+    for index, video in enumerate(videos, start=1):
+        print(f"{index}.")
+
+
+def add_video(videos):
+    name = input("Enter a video name:  ")
+    time = input("Enter a video time: ")
+    videos.append({'name': name , 'time': time})
+    save_data_helper(videos)
+
+
+def update_video(videos):
+    pass
+
+def delete_video(videos):
+    pass
+
+def exit_video(videos):
+    pass
+
+
+def main():
+    videos = load_data() 
+    while True:
+        print("\n Youtube Manager")
+        print("1. List all youtube videos ")
+        print("2. Add a youtube video ")
+        print("3. Update a youtube video details ")
+        print("4. Delete a youtube video ")
+        print("5. Exit the app ")
+        choice = input("Enter your choice:  ")
+        print(videos)
+
+        match choice:
+            case '1':
+                list_all_videos(videos)
+            case '2':
+                add_video(videos)
+            case '3':
+                update_video(videos)
+            case '4':
+                delete_video(videos)
+            case '5':
+                exit_video(videos)
+                break
+            case _:
+                print(" InValid choice : ")
+
+
+
+if __name__ == "__main__":
+    main()        
